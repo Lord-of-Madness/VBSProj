52e349e634b8e83b3c4614bb409c5c99cc7057fa
Lord of Madness <zemekoulak@gmail.com>
2024-12-14 12:52:19 +0100

    Adding the Sequential advance trees before I mess something up

---
+++ data/behaviors_DPKR_occupy/DPKRsq/DPKRsq.class.lua
@@ -0,0 +1,33 @@
+return {
+    DPKRsq = {
+        description = 'An extension to Infantrysquad. A covert DPRK squadron',
+        parents = {
+            { btset = 'standard_behaviors', class = 'InfantrySquad' },
+            -- More parent classes can be added. The 'btset' field is optional and defaults to the current project.
+        },
+        
+        Match = function ()
+        -- This class matches the same objects as the parent class does.
+        -- You can add a condition matching only subset of soldiers.
+            if
+                type(self) == "GroupBrain"
+            then
+                return true
+            end        
+            return false
+        end,
+
+        behaviors = {
+            Idle = {
+                description = 'Idle',
+                tree = 'Chill',
+                parameters = {},
+            },
+            Custom = {
+                description = 'Custom',
+                tree = 'Occupy',
+                parameters = {},
+            }
+        },
+    },
+}
